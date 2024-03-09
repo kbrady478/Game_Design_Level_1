8d97ff3e1e8940d6020b4b2c93942ad43d179683
kbrady478 <c23789389@mytudublin.ie>
2024-03-09 20:30:37 +0000

    Finally got shield hits registering
    This has taken years off of my life

--- Level_Design_1/Assets/Scripts/Player_Shield.cs
+++ Level_Design_1/Assets/Scripts/Player_Shield.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player_Shield : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //private bool shield_Toggle = false;
+    public GameObject repulsor_Shield;
+    public GameObject projectile;
+
+    private void Start()
     {
-        
+        if (repulsor_Shield.activeInHierarchy == true)
+        {
+           // repulsor_Shield.SetActive(false);
+        }
+    }// end Start()
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown((int)MouseButton.Right))
+        {
+            Shield_On();
+        }
+
+        if (Input.GetMouseButtonUp((int)MouseButton.Right))
+        {
+            Shield_Off();
+        }
+    }// end Update()
+
+    private void OnCollisionEnter(Collision collided)
+    {
+
+        if (collided.gameObject.CompareTag("Bastard_Projectile"))
+        {
+            Destroy(gameObject);
+            print("projectile blocked");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Shield_On()
     {
-        
-    }
-}
+        print("shield on"); 
+        repulsor_Shield.SetActive(true);
+        //shield_Toggle = true;
+
+
+    }// end Shield_On()
+
+    private void Shield_Off()
+    {
+        print("shield off");
+        repulsor_Shield.SetActive(false);
+        //shield_Toggle = false;
+    }// end Shield_Off()
+    
+}// end Player_Shield
