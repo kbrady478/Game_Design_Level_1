38c82f1c4e96e1e433ffd8d0d19b34616df81395
kbrady478 <c23789389@mytudublin.ie>
2024-03-14 23:22:04 +0000

    Revert "Player death + restart working"
    This reverts commit 3fc96a8f9cd6db3b22e4936d300b29692f5c6258.

--- Level_Design_1/Assets/Scripts/Player_Stats.cs
+++ Level_Design_1/Assets/Scripts/Player_Stats.cs
@@ -1,30 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
 
 public class Player_Stats : MonoBehaviour
 {
-    public MonoBehaviour player_Movement_Script;
-    public MonoBehaviour player_Shield_Script;
-    
     public int hit_Points = 100;
     public int damage_Recieved = 30;
     public TextMeshProUGUI hit_Points_UI;
-
-    public TextMeshProUGUI shield_Energy_UI;
-    public GameObject death_Screen_UI;
+    
     
     void Update()
     {
-
-        
-        if (hit_Points <= 0)
-            trigger_Death();
-        
         hit_Points_UI.text = "HP: " + hit_Points;
     }
 
@@ -32,24 +20,4 @@ public class Player_Stats : MonoBehaviour
     {
         hit_Points -= damage_Recieved;
     }
-
-    public void trigger_Death()
-    {
-        player_Movement_Script.enabled = false;
-        player_Shield_Script.enabled = false;
-        shield_Energy_UI.enabled = false;
-        hit_Points_UI.enabled = false;
-        death_Screen_UI.SetActive(true);
-        
-        if (Input.GetKeyDown(KeyCode.R))
-            restart_Level();
-    }
-    
-    public void restart_Level()
-    {
-        int current_Scene = SceneManager.GetActiveScene().buildIndex;
-
-        SceneManager.LoadScene(current_Scene);
-    }
-    
 }
